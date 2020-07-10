58f1a6d701dc7a3ac852fae6688b9e2491cb9652
ElContent <61986615+ElContent@users.noreply.github.com>
2020-07-10 12:32:55 +0200

    Create compConM1.m

---
+++ Grafos/compConM1.m
@@ -0,0 +1,13 @@
+function compConM1(mAdyacencia)
+    [M, N] = size(mAdyacencia);
+    if M ~= N
+        error("Matriz inválida")
+    end
+    V = zeros(1, N);
+    
+    for i = 1:N
+        
+    end
+    
+end
+
