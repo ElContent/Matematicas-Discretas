cf4a185b290f07ba5a0f6b90c98f2e9ee001ec3b
ElContent <61986615+ElContent@users.noreply.github.com>
2020-07-16 00:46:54 +0200

    actualización

--- Grafos/algoritmoFloydWarshall.m
+++ Grafos/algoritmoFloydWarshall.m
@@ -1,52 +1,53 @@
-function algoritmoFloydWarshall()
-
-% Introducimos matriz de pesos:
-mPesos = input("Introduce matriz de pesos (pon los infinitos como Inf): ");
-N = size(mPesos, 1);
-mRecorridos = zeros(N);
-
-% % Montando la tabla de recorridos:
-for i = 1:N
-    for j = 1:N
-        if mPesos(i, j) ~= Inf
-            mRecorridos(i, j) = i;
-        end
+function algoritmoFloydWarshall(M_in)
+    if exist('M_in') == true
+        mPesos = M_in;
+    else
+        % Introducimos matriz de pesos:
+        mPesos = input("Introduce matriz de pesos (pon los infinitos como Inf): ");
+        
     end
-end
-
-% Paso 1:
-paso = 1;
-fprintf("\n## Paso %d ## \n", paso);
-
-fprintf("Matriz de pesos: \n");
-disp(mPesos);
-
-fprintf("Matriz de recorridos: \n");
-disp(mRecorridos);
-
-% Operando:
-for i = 1:N
-    fprintf("\n## Paso %d ## \n", i + 1);
-    mPesos_anterior = mPesos;
-    mPesos = min(mPesos, mPesos(:, i) + mPesos(i, :));
     
-    for r = 1:N
-        for s = 1:N
-            if mPesos(r, s) ~= mPesos_anterior(r, s)
-                mRecorridos(r, s) = mRecorridos(i, s);
+    N = size(mPesos, 1);
+    mRecorridos = zeros(N);
+
+    % % Montando la tabla de recorridos:
+    for i = 1:N
+        for j = 1:N
+            if mPesos(i, j) ~= Inf
+                mRecorridos(i, j) = i;
             end
         end
     end
-    
-    fprintf("Matriz de pesos (modificada) \n");
+
+    % Paso 1:
+    paso = 1;
+    fprintf("\n## Paso %d ## \n", paso);
+
+    fprintf("Matriz de pesos: \n");
     disp(mPesos);
-    
-    fprintf("Matriz de recorridos \n");
+
+    fprintf("Matriz de recorridos: \n");
     disp(mRecorridos);
-end
 
-
-
-
+    % Operando:
+    for i = 1:N
+        fprintf("\n## Paso %d ## \n", i + 1);
+        mPesos_anterior = mPesos;
+        mPesos = min(mPesos, mPesos(:, i) + mPesos(i, :));
+    
+        for r = 1:N
+            for s = 1:N
+                if mPesos(r, s) ~= mPesos_anterior(r, s)
+                    mRecorridos(r, s) = mRecorridos(i, s);
+                end
+            end
+        end
+    
+        fprintf("Matriz de pesos (modificada) \n");
+        disp(mPesos);
+    
+        fprintf("Matriz de recorridos \n");
+        disp(mRecorridos);
+    end
 end
 
